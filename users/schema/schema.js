d903136f7cfde11dd4231eaffc5ca4fff156a1d7
Zil Vyas <zvyas08@gmail.com>
2022-12-22 12:04:30 -0500

    initial setup

---
+++ users/schema/schema.js
@@ -0,0 +1,13 @@
+const graphql = require('graphql');
+const {
+    GraphQLObjectType
+} = graphql;
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: graphql.GraphQLString },
+        firstName: { type: graphql.GraphQLString },
+        age: { type: graphql.GraphQLInt } 
+    }
+});
