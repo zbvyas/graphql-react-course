1a4f63947c00742197dd68cb79c4dbd82499aef7
Zil Vyas <zvyas08@gmail.com>
2022-12-22 12:26:45 -0500

    wire up graphiql

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,7 +1,17 @@
 const graphql = require('graphql');
+const _ = require('lodash');
+
 const {
-    GraphQLObjectType
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema
 } = graphql;
+
+const users = [
+    { id: '23', firstName: 'Bill', age: 20 },
+    { id: '47', firstName: 'Samantha', age: 21 },
+];
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -10,4 +20,21 @@ const UserType = new GraphQLObjectType({
         firstName: { type: graphql.GraphQLString },
         age: { type: graphql.GraphQLInt } 
     }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: graphql.GraphQLString } },
+            resolve(parentValue, args) {
+                return _.find(users, { id: args.id });
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
