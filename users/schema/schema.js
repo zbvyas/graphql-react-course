4e0af9715eab8932a01a4a71938c331f2dce6e00
Zil Vyas <zvyas08@gmail.com>
2022-12-23 10:09:28 -0500

    add company to the user type resolver

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -8,12 +8,28 @@ const {
     GraphQLSchema
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+});
+
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
-        id: { type: graphql.GraphQLString },
-        firstName: { type: graphql.GraphQLString },
-        age: { type: graphql.GraphQLInt } 
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        company: {
+            type: CompanyType,
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                    .then(resp => resp.data);
+            }
+        }
     }
 });
 
@@ -22,7 +38,7 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         user: {
             type: UserType,
-            args: { id: { type: graphql.GraphQLString } },
+            args: { id: { type: GraphQLString } },
             resolve(parentValue, args) {
                 return axios.get(`http://localhost:3000/users/${args.id}`)
                     .then(resp => resp.data);
