800e06a361031331651a245d60cb23615a51e148
Zil Vyas <zvyas08@gmail.com>
2022-12-29 14:19:16 -0500

    initial song list component

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({});
+
+// Custom Components
+import SongList from './components/SongList';
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <SongList />
+    </ApolloProvider>
+  )
 };
 
 ReactDOM.render(
