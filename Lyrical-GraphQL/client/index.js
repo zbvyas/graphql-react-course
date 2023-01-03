29a2bce27799a631ed8806a684bd6e81782e22ff
Zil Vyas <zvyas08@gmail.com>
2023-01-03 11:37:48 -0500

    add index route

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({});
 
 // Custom Components
+import App from './components/App';
 import SongList from './components/SongList';
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList}></IndexRoute>
+        </Route>
+      </Router>
     </ApolloProvider>
   )
 };
